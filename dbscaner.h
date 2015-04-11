f17140c4447e5efd0aa60f85ef56350f02945404
hongyiz910417 <hongyiz0417@gmail.com>
2015-04-11 01:46:40 -0400

    without debugging

--- dbscaner.h
+++ dbscaner.h
@@ -1,2 +1,4 @@
 void load_data(char* path);
 void print_data();
+void cluster(char* outpath, double eps, int minPnts, double diff);
+void print_clusters();
--- main.c
+++
@@ -1,6 +0,0 @@
-#include "dbscaner.h"
-
-int main(int argc, char **argv){
-	load_data("/Users/d/Desktop/test");
-	print_data();
-}
